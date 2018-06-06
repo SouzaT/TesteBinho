5e59f52c26603b8b1e2fa7567b924cb1b95ea16c
Souza <TOSA@gft.com>
2018-06-06 12:52:36 -0300

    Primeiro Commit

---
+++ BinhoHelloWorld/BinhoHelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinhoHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
